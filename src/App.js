02d70e6bcd4b26744f81ddc21377ebf7f71abdb0
dima016 <Unlimited_4_all!>
2022-08-29 19:15:36 +0300

    Updated view and API url

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
 
+
+  useEffect(() => {
+    fetch("http://172.31.23.18/messages")
+        .then(res => res.json())
+        .then(
+            (result) => {
+              setIsLoaded(true);
+              setItems(result);
+            },
+            (error) => {
+              setIsLoaded(true);
+              setError(error);
+            }
+        )
+  }, [])
+
+  if (error) {
+    return <div>Ошибка: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Загрузка...</div>;
+  } else {
+    return (
+        <ul>
+          {items.map(item => (
+              <li key={item.id}>
+                {item.id}: {item.message}
+              </li>
+          ))}
+            Hello from React App
+        </ul>
+    );
+  }
+}
 export default App;
